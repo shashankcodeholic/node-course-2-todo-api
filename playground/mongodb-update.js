5176016882629a4bbc5cc4dbded65cb0c82be70f
SHASHANK-PC\SHASHANK <shashank@codeholic.in>
2019-01-22 14:04:48 +0530

    add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+const {MongoClient, ObjectID}  = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+	if (err) {
+		return console.log('Unable to connect to MongoDB server');
+	}
+	console.log('Connected to MongoDB server');
+	const db = client.db('TodoApp');
+
+	// db.collection('Todos').findOneAndUpdate({
+	// 	_id: new ObjectID('5c46b5437c9ad36ff6fb2c0d')
+	// },{
+	// 	$set: {
+	// 		completed: true
+	// 	}
+	// },{
+	// 	returnOriginal: false
+	// }).then((result) => {
+	// 	console.log(result);
+	// });
+
+	db.collection('Users').findOneAndUpdate({
+		name: 'Shashank'
+	},{
+		$set: {
+			name: 'Tanmay'
+		},
+		$inc: {
+			age: 2
+		}
+	},{
+		returnOriginal : false
+	}).then((result) => {
+		console.log(result);
+	});
+	
+	client.close();
+});
